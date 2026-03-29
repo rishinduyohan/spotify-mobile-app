8076fc36bb39305f09d547f2fda833aeb817eb22
Rishindu Yohan <rishinduyohan@gmail.com>
2026-03-30 02:44:50 +0530

    Add splash screen functionality and create GetStartedPage with background image

---
+++ lib/presantation/intro/pages/get_started.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotify/common/widgets/button/basic_button.dart';
+import 'package:spotify/core/configs/assets/app_images.dart';
+import 'package:spotify/core/configs/assets/app_vectors.dart';
+import 'package:spotify/core/configs/theme/app_colors.dart';
+
+class GetStartedPage extends StatelessWidget {
+  const GetStartedPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 50, horizontal: 50),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(AppImages.introBG),
+                fit: BoxFit.fill,
+              ),
+            ),
+
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: SvgPicture.asset(AppVectors.spotifyLogo),
+                ),
+                const Spacer(),
+                Text(
+                  'Millions of songs. Free on Spotify.',
+                  style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                SizedBox(height: 21),
+                Text(
+                  'Join millions of listeners and creators. From the hits you love to the underground tracks you’ve yet to find, everything you need to hear is right here.',
+                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                    color: AppColors.grey,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 18, 
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 25),
+                BasicButton(onPressed: (){}, title: "Get Started", height: 60)
+              ],
+            ),
+          ),
+          Container(color: Colors.black.withValues(alpha: 0.250)),
+        ],
+      ),
+    );
+  }
+}
--- lib/presantation/splash/pages/splash.dart
+++ lib/presantation/splash/pages/splash.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:spotify/core/configs/assets/app_vectors.dart';
+import 'package:spotify/presantation/intro/pages/get_started.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+
+  @override
+  void initState(){
+    super.initState();
+    redirectToHome();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,5 +27,10 @@ class SplashPage extends StatelessWidget {
         )
       ),
     );
+  }
+
+  Future<void> redirectToHome() async {
+    await Future.delayed(const Duration(seconds: 3));
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (BuildContext context) => const GetStartedPage()));
   }
 }
