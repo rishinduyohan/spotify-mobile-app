52d1f96c5401738439b9d48b11c62cffc68a27c6
Rishindu Yohan <rishinduyohan@gmail.com>
2026-03-29 19:13:57 +0530

    Add AppColors and AppTheme classes for color management and theming

---
+++ lib/core/configs/theme/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+    static const Color primary = Color(0xFF1DB954);
+    static const Color lightbackground = Color.fromARGB(255, 242, 255, 247);
+    static const Color darkbackground = Color(0xFF191414);
+    static const Color grey = Color.fromARGB(255, 122, 120, 120);
+    static const Color darkgrey = Color.fromARGB(255, 50, 50, 50);
+}
